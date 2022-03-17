365ab0145b785cd84458baf19cb8522241dc302c
Leonidas Spyropoulos <artafinde@gmail.com>
2022-03-17 17:51:02 +0000

    build: add checkstyle plugin
    Includes checks for:
    - ordering of imports
    - consistent indentation
    - line length size
    - file for suppression of rules
    
    Signed-off-by: Leonidas Spyropoulos <artafinde@gmail.com>

--- src/main/java/com/thekliar/reactive/handler/BlogHandler.java
+++ src/main/java/com/thekliar/reactive/handler/BlogHandler.java
@@ -2,14 +2,13 @@ package com.thekliar.reactive.handler;
 
 import static com.thekliar.reactive.config.AppConstants.ID;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
-
 import com.thekliar.reactive.model.Blog;
 import com.thekliar.reactive.model.QBlog;
 import com.thekliar.reactive.repository.BlogRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @Component
