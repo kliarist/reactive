8a3fb9283aa3aeb316aa3034fbd41f464c6d70b3
Theo Kliaris <theodoros.kliaris@gmail.com>
2022-03-20 10:07:07 +0200

    feat: add support for reactive mongo auditing

--- src/main/java/com/thekliar/reactive/config/MongoConfig.java
+++ src/main/java/com/thekliar/reactive/config/MongoConfig.java
@@ -1,9 +1,19 @@
 package com.thekliar.reactive.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.ReactiveAuditorAware;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableReactiveMongoRepositories(basePackages = "com.thekliar.reactive.repository")
+@EnableReactiveMongoAuditing
 public class MongoConfig {
+
+  @Bean
+  public ReactiveAuditorAware<String> auditorProvider() {
+    return () -> Mono.just("system");
+  }
 }
