467bfec102c12c88b0c5a3491c48eadb3f217c51
Theo Kliaris <theodoros.kliaris@gmail.com>
2022-03-20 10:10:33 +0200

    feat: add BaseRepository abstraction and BlogRepository subclass

---
+++ src/main/java/com/thekliar/reactive/repository/BaseRepository.java
@@ -0,0 +1,11 @@
+package com.thekliar.reactive.repository;
+
+import com.thekliar.reactive.model.BaseDocument;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.querydsl.ReactiveQuerydslPredicateExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<D extends BaseDocument>
+    extends ReactiveMongoRepository<D, String>,
+    ReactiveQuerydslPredicateExecutor<D> {}
