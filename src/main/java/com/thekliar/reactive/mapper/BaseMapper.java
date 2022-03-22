4dfcfab59df057a6afb76e3b846f6bd92b37f136
Theo Kliaris <theodoros.kliaris@gmail.com>
2022-03-22 22:16:57 +0200

    feat: no need for repository injection

--- src/main/java/com/thekliar/reactive/mapper/BaseMapper.java
+++ src/main/java/com/thekliar/reactive/mapper/BaseMapper.java
@@ -2,16 +2,11 @@ package com.thekliar.reactive.mapper;
 
 import com.thekliar.reactive.dto.BaseDto;
 import com.thekliar.reactive.model.BaseDocument;
-import com.thekliar.reactive.repository.BaseRepository;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Flux;
 
 public abstract class BaseMapper<D extends BaseDto, T extends BaseDocument> {
-
-  @Autowired
-  BaseRepository<T> repository;
 
   @Mapping(ignore = true, target = "modifiedBy")
   @Mapping(ignore = true, target = "modifiedOn")
