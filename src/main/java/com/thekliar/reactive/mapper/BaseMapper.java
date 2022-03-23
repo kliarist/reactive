19f537c92639c31e2b4b52681de9df774f299ea0
Theo Kliaris <theodoros.kliaris@gmail.com>
2022-03-23 12:31:47 +0200

    refactor: use interface instead of abstracrt class

--- src/main/java/com/thekliar/reactive/mapper/BaseMapper.java
+++ src/main/java/com/thekliar/reactive/mapper/BaseMapper.java
@@ -6,30 +6,30 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import reactor.core.publisher.Flux;
 
-public abstract class BaseMapper<D extends BaseDto, T extends BaseDocument> {
+public interface BaseMapper<D extends BaseDto, T extends BaseDocument> {
 
   @Mapping(ignore = true, target = "modifiedBy")
   @Mapping(ignore = true, target = "modifiedOn")
   @Mapping(ignore = true, target = "createdBy")
   @Mapping(ignore = true, target = "createdOn")
   @Mapping(ignore = true, target = "version")
-  public abstract T toDocument(D dto);
+  T toDocument(D dto);
 
-  public abstract D toDto(T document);
+  D toDto(T document);
 
   @Mapping(ignore = true, target = "modifiedBy")
   @Mapping(ignore = true, target = "modifiedOn")
   @Mapping(ignore = true, target = "createdBy")
   @Mapping(ignore = true, target = "createdOn")
   @Mapping(ignore = true, target = "version")
-  public abstract void map(D dto, @MappingTarget T document);
+  void map(D dto, @MappingTarget T document);
 
-  public Flux<D> toDtos(Flux<T> all) {
+  default Flux<D> toDtos(Flux<T> all) {
     return all.map(this::toDto);
   }
 
   @SuppressWarnings("unused")
-  public Flux<T> toDocuments(Flux<D> all) {
+  default Flux<T> toDocuments(Flux<D> all) {
     return all.map(this::toDocument);
   }
 
--- src/main/java/com/thekliar/reactive/mapper/BlogMapper.java
+++ src/main/java/com/thekliar/reactive/mapper/BlogMapper.java
@@ -5,6 +5,6 @@ import com.thekliar.reactive.model.Blog;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public abstract class BlogMapper extends BaseMapper<BlogDto, Blog> {
+public interface BlogMapper extends BaseMapper<BlogDto, Blog> {
 
 }
